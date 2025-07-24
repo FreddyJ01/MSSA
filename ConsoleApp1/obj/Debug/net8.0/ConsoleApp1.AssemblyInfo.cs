3dd1681abb223a05ddf4d7042189bf4ab73411ba
FreddyJ01 <fahrodej01@gmail.com>
2025-07-24 15:23:04 -0500

    GridMaker

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c02f79488c1dd8e777d048612fd50e77121e4bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f6dd132eddc935df33567dc5ac585aa660aa0a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
