77d49476381fe1d8acc3557f004ab7abed65ee96
FreddyJ01 <fahrodej01@gmail.com>
2025-07-28 13:00:10 -0500

    WhileLoops

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9f3f4dc80cb1b6a772d87b8ee65f32809994a62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccf7a7eb1ea27aae4ad4ce1af7d0050e7abccb2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
