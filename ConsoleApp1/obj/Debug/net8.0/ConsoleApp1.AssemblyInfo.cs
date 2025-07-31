aa135b76211a61b2e82e02d778cd2b545ef4fd9b
FreddyJ01 <fahrodej01@gmail.com>
2025-07-31 12:38:32 -0500

    String and Array Manipulation

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de5bbd195068dd48b14f61ab1ccc6e40840826e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d05881f2cfa7b9f37532e57755e84371a0a88465")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
