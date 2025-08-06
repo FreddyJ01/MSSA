5ce64219d75ad3cb70fc900c168767541edb521d
FreddyJ01 <fahrodej01@gmail.com>
2025-08-06 10:47:52 -0500

    Padding And Alignment

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa135b76211a61b2e82e02d778cd2b545ef4fd9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ead66c2dcf262f688c49dd7d8987133576f9977")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
