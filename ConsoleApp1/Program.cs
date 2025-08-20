efc284fe63c7b8d6f840edcffbd41df6dfce30c1
FreddyJ01 <fahrodej01@gmail.com>
2025-08-20 09:37:48 -0500

    ArrayLists

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] Args)
     {
-        
+        ArrayList list = new ArrayList();
+        list.Add("apple");
+
+        System.Console.WriteLine();
     }
 }
