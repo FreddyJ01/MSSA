191a3097e772e4fcafe47aadb09ab3889c01652f
FreddyJ01 <fahrodej01@gmail.com>
2025-09-09 09:09:25 -0500

    BigONotation

--- AAA/Program.cs
+++ AAA/Program.cs
@@ -11,8 +11,42 @@ class Program
 {
     static void Main()
     {
-        int a = 10;
-        int b = 20;
-        System.Console.WriteLine("Sum is " + Operations.Sum(a, b).ToString());
+        int[] arr = { 1, 22, 3, 4, 52, 6, 7 };
+        int[] newArr = new int[arr.Length + 1];
+        int insertIndex = 3;
+        int newValue = 99;
+
+        for (int i = 0; i < insertIndex; i++)
+            newArr[i] = arr[i];
+
+        newArr[insertIndex] = newValue;
+
+        for (int i = insertIndex; i < arr.Length; i++)
+            newArr[i + 1] = arr[i];
+
+        foreach (int num in newArr)
+            Console.WriteLine(num);
+
+        int[] newerArr = new int[arr.Length - 1];
+        int deleteIndex = 2;
+
+        for (int i = 0; i < newerArr.Length; i++)
+        {
+
+            if (i == deleteIndex)
+            {
+                newerArr[i] 
+            }
+
+            newerArr[i] = arr[i];
+        }
+        
+        System.Console.WriteLine();
+
+        foreach (int num in newerArr)
+        {
+            System.Console.WriteLine(num);
+        }
+
     }
 }
